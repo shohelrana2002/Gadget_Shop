ac6b57426e1ed54e62003233e1aed5ff4fa784e4
shohelrana2002 <rana2021tang@gmail.com>
2024-11-15 13:43:06 +0600

    Add to authProvider and authcontect value ,value info in children and add to onauthstateChanged

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,36 +1,66 @@
-import React, { createContext } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import app from "../Firebase/Firebase";
 import {
   createUserWithEmailAndPassword,
   getAuth,
+  onAuthStateChanged,
   signInWithEmailAndPassword,
   signInWithPopup,
   signOut,
 } from "firebase/auth";
 import { GoogleAuthProvider } from "firebase/auth/web-extension";
 export const AuthContext = createContext(null);
+
 const auth = getAuth(app);
-const AuthProvider = () => {
+const AuthProvider = ({ children }) => {
+  // state set
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // google auth provider set
   const googleProvider = new GoogleAuthProvider();
 
+  // create a New Account
   const createAccount = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
-  const loginUser = (email, password) => {
+  // User Login
+  const userLogin = (email, password) => {
     return signInWithEmailAndPassword(auth, email, password);
   };
+
+  // Google login
   const googleLogin = () => {
     return signInWithPopup(googleProvider, auth);
   };
+
+  // User logOut
   const userLogout = () => {
     return signOut(auth);
   };
 
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => {
+      return unSubscribe();
+    };
+  }, []);
+
+  // sent to context
+  const authInfo = {
+    user,
+    loading,
+    createAccount,
+    userLogin,
+    userLogout,
+    googleLogin,
+  };
   return (
-    <div>
-      <p>AuthProvider</p>
-    </div>
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
 };
 
