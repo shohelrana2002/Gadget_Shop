8b641decbbbf754bbe8c7a7cea95879f2df2cafa
shohelrana2002 <rana2021tang@gmail.com>
2024-11-20 21:01:36 +0600

    Add to featcuread sestion

--- src/components/Home/FeaturedProducts.jsx
+++ src/components/Home/FeaturedProducts.jsx
@@ -7,11 +7,6 @@ const FeaturedProducts = () => {
       <ProductsCards></ProductsCards>
       <ProductsCards></ProductsCards>
       <ProductsCards></ProductsCards>
-      <ProductsCards></ProductsCards>
-      <ProductsCards></ProductsCards>
-      <ProductsCards></ProductsCards>
-      <ProductsCards></ProductsCards>
-      <ProductsCards></ProductsCards>
     </div>
   );
 };
