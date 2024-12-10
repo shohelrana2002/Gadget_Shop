f8fd8e4f8a1cf7c8fbbc275af12f8c2e6bcb5445
shohelrana2002 <rana2021tang@gmail.com>
2024-12-10 23:03:36 +0600

    Add ro bMain Layout stion

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -25,6 +25,7 @@ const MainLayout = () => {
         <Outlet></Outlet>
         <Outlet></Outlet>
         <Outlet></Outlet>
+        <Outlet></Outlet>
       </div>
       <Footer></Footer>
     </div>
