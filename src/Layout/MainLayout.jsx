f2ca05d39f1f1330bf58ff666a1228620746d3d2
shohelrana2002 <rana2021tang@gmail.com>
2024-12-16 21:59:10 +0600

    Add ro bMain Layout stion

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -36,16 +36,7 @@ const MainLayout = () => {
       <Footer>1</Footer>
       <Footer>1</Footer>
       <Footer>1</Footer>
-      <Footer>1</Footer>
-      <Footer>1</Footer>
-      <Footer>1</Footer>
-      <Footer>1</Footer>
-      <Footer>1</Footer>
-      <Footer>1</Footer>
-      <Footer>1</Footer>
-      <Footer>1</Footer>
-      <Footer>1</Footer>
-      <Footer>1</Footer>
+
       <Footer>1</Footer>
     </div>
   );
