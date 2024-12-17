8f5d91eec90e27418e1c2ab85b10a13ab698ffd9
shohelrana2002 <rana2021tang@gmail.com>
2024-12-17 22:59:32 +0600

    add To Navbar section

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -7,6 +7,7 @@ const MainLayout = () => {
     <div>
       <Navbar></Navbar>
       <Navbar></Navbar>
+      <Navbar></Navbar>
       <div className="min-h-screen-minus-240 pt-16">
         <Outlet></Outlet>
       </div>
