c930f51e325b555405b2c7d2ecb3ba55dfb212f1
shohelrana2002 <rana2021tang@gmail.com>
2024-12-18 20:49:55 +0600

    Add ro bMain Layout stion

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -6,8 +6,7 @@ const MainLayout = () => {
   return (
     <div>
       <Navbar></Navbar>
-      
-      <Navbar></Navbar>
+
       <div className="min-h-screen-minus-240 pt-16">
         <Outlet></Outlet>
       </div>
