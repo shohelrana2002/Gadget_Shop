1933a42136f37b421a671780ce3af0f1d8f40655
shohelrana2002 <rana2021tang@gmail.com>
2024-12-19 21:44:22 +0600

    Add To Error Page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -4,6 +4,7 @@ const Error = () => {
       <h3 className="text-4xl text-rose-700">
         Page Not Found <span className="text-5xl text-gray-900">404</span>
       </h3>
+      <p>Back To Home Page</p>
     </div>
   );
 };
