e8e49a3e54bc75770954d4c4366b5eb1b90aa248
shohelrana2002 <rana2021tang@gmail.com>
2024-12-19 21:43:02 +0600

    Add To Error Page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -2,7 +2,7 @@ const Error = () => {
   return (
     <div className="min-h-screen items-center flex justify-center text-center">
       <h3 className="text-4xl text-rose-700">
-        Page Not Found <span>404</span>
+        Page Not Found <span className="text-5xl text-gray-900">404</span>
       </h3>
     </div>
   );
